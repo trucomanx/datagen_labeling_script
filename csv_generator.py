8443e04a79d39c4b5dfdfc6858e7aedede98be19
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-03-22 19:07:34 -0300

    ordenar

--- csv_generator.py
+++ csv_generator.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python
 
-import sys
-sys.path.append('/mnt/c/Dados/Fernando/CODE/PESQUISA/libraries/WorkingWithFiles/src');
+## pip3 install natsort
 
+import sys
+sys.path.append('../../libraries/WorkingWithFiles/src');
 import WorkingWithFiles as rnfunc
 
-csv_path='labels.csv';
+#######################################################################
+## python csv_generator.py --format .png --csv-file labels.csv --base-dir /my/path
+#######################################################################
+csv_file='labels.csv';
 base_dir='/mnt/c/Dados/Fernando/DATASET/fer2013-fpr2023/archive/test';
+default_format='.png';
 
-format_list=['.png'];
+format_list=[];
+for n in range(len(sys.argv)):
+    if sys.argv[n]=='--csv-file':
+        csv_file=sys.argv[n+1];
+    if sys.argv[n]=='--base-dir':
+        base_dir=sys.argv[n+1];
+    if sys.argv[n]=='--format':
+        format_list.append(sys.argv[n+1]);
 
-rnfunc.generate_csv_file_from_dir_structure(base_dir,format_list,csv_path);
+if len(format_list)==0:
+    format_list=[default_format];
+
+print('')
+print('   csv_file:',csv_file)
+print('   base_dir:',base_dir)
+print('format_list:',format_list)
+print('')
+
+rnfunc.generate_csv_file_from_dir_structure(base_dir,format_list,csv_file);
